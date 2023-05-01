8288af330beadc8105b0020701677f9aeba5d092
Totr <110344016+TotrCryp@users.noreply.github.com>
2023-05-01 16:22:58 +0300

    Create totr_logo.sh

---
+++ totr_logo.sh
@@ -0,0 +1,12 @@
+echo -e "\e[40m\e[94m"
+echo -e "                                                         ";
+echo "    TTTTTTTTTTTTTTTTTT";
+echo "   TTTTTTTTTTTTTTTTTT";
+echo "         TTTT         TTTTTTTT       TTT         TT TTTT";
+echo "        TTTT         TTT   TTT     TTT          TTTT  TTT";
+echo "       TTTT         TTT   TTT   TTTTTTTT       TTT";
+echo "      TTTT         TTT   TTT      TTT         TTT";
+echo "     TTTT         TTT   TTT      TTT   TTT   TTT";
+echo "   TTTTTTT        TTTTTTTT        TTTTTTT   TTT";
+echo "";
+echo -e "\e[0m"
